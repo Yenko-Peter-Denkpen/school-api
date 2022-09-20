f880f4867cc797af3a22bb0057f0a985c505656d
Yenko-Peter-Denkpen <110115346+Yenko-Peter-Denkpen@users.noreply.github.com>
2022-09-20 13:49:57 +0000

    updated

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,13 +1,11 @@
-const mongoose = require("mongoose");
-
+const mongoose =require("mongoose");
 
 const connectDB = async () => {
     await mongoose.connect(process.env.DB_URL, {
-        useNewUrlparser: true,
-        useUnifiedTopology:true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
 
     })
     console.log("database is connected")
 }
-
-module.exports = connectDB;
+module.exports= connectDB;
